7e88837719764200d3e16fc5f0487c6f1f8b09b9
AroOsw <128989508+AroOsw@users.noreply.github.com>
2024-03-14 19:06:54 +0100

    add_web_page

--- Web_page/main.py
+++ Web_page/main.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, redirect, url_for, send_from_directory
-#from flask import Bootstrap5
 
 
 app = Flask(__name__)
-# app.config["SECRET_KEY"] = "Super_secret_key"
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route('/download')
 def download():
